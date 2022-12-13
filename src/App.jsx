b6cd0f68ee39cf0b487f98c043e489100f4a0f87
Lucas <lucas.ffr2014@gmail.com>
2022-12-12 22:29:14 -0300

    Add Absolute imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Routes from 'routes';
 import Toast from 'components/Toast';
 import { UserContextProvider } from 'contexts/user';
+import Routes from 'routes';
 
 function App() {
   return (
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = document.getElementById('root');
+if (!element) {
+  throw new Error("The element wasn't found");
+}
+const root = ReactDOM.createRoot(element);
 root.render(<App />);
